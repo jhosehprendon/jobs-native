40fc53e02ff6e581e9fd0cfe1e1c27ff54e77d08
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2018-12-12 10:03:31 -0500

    Rendering swiping cards

---
+++ actions/job_actions.js
@@ -0,0 +1,36 @@
+// import axios from 'axios'; 
+// import qs from 'qs';
+// // import { Location } from 'expo';
+import JOB_DATA from './indeedJobData.json';
+
+import { FETCH_JOBS } from './types';
+
+// const JOB_ROOT_URL = 'https://jobs.github.com/positions.json?' 
+
+// const JOB_QUERY_PARAMS = {
+//     publisher: '4201738803816157',
+//     format: 'json', 
+//     v: '2',
+//     latlong: 1,
+//     radius: 10,
+//     q: 'javascript'
+// }
+
+// const buildJobsUrl = (zip) => {
+//     const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip })
+//     return `${JOB_ROOT_URL}${query}`
+// }
+
+export const fetchJobs = (region, callback) => async (dispatch) => {
+    try {
+      //let zip = await reverseGeoCode(region);
+      //const url = buildJobsUrl(zip);
+      //let { data } = await axios.get(url);
+      const data = JOB_DATA;
+      //console.log(data);
+      dispatch({ type: FETCH_JOBS, payload: data });
+      callback();
+    } catch (e) {
+      console.log(e);
+    }
+  };
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { View, Text, ActivityIndicator } from 'react-native';
-import { MapView } from 'expo'
+import { MapView, Permissions } from 'expo';
+import { connect } from 'react-redux';
+import { Button } from 'react-native-elements';
+
+import * as actions from '../actions'
 
 class MapScreen extends Component {
 
@@ -14,8 +18,21 @@ class MapScreen extends Component {
         }
     }
 
-    componentDidMount() {
+    async componentDidMount() {
+        await Permissions.askAsync(Permissions.LOCATION);
         this.setState({ mapLoaded: true })
+    }
+
+    onRegionChangeComplete = (region) => {
+        this.setState({ region })
+    }
+
+    onButtonPress = () => {
+        this.props.fetchJobs(this.state.region, () => {
+
+            this.props.navigation.navigate('deck');
+      
+          });
     }
 
     render() {
@@ -31,10 +48,29 @@ class MapScreen extends Component {
                 <MapView 
                     style={{ flex: 1 }}
                     region={this.state.region} 
+                    onRegionChangeComplete={this.onRegionChangeComplete}
                 />
+                <View style={styles.buttonContainer}>
+                    <Button 
+                        large
+                        title='Search this area'
+                        backgroundColor= '#009688'
+                        icon={{ name: 'search' }}
+                        onPress={this.onButtonPress}
+                    />
+                </View>
             </View>
         )
     }
 }
 
-export default MapScreen
+const styles = {
+    buttonContainer: {
+        position: 'absolute',
+        bottom: 20,
+        left: 0,
+        right: 0
+    }
+}
+
+export default connect(null, actions)(MapScreen)
