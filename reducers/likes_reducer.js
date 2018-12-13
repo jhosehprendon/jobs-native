836f94176074906d6aeb09acdb9de357ef6f5bc6
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2018-12-13 06:55:17 -0500

    Offline data persistance

--- reducers/likes_reducer.js
+++ reducers/likes_reducer.js
@@ -1,11 +1,15 @@
 import _ from 'lodash';
+import { REHYDRATE } from 'redux-persist/constants'
 import {
     LIKE_JOB,
     CLEAR_LIKED_JOBS
 } from '../actions/types';
+import { fromBinary } from 'uuid-js';
 
 export default function(state = [], action) {
     switch(action.type) {
+        case REHYDRATE:
+            return action.payload.likedJobs || []
         case LIKE_JOB:
             return _.uniqBy([
                 action.payload,
